a626c1fdf1c533039b494a1b6e241f2d63f22352
MarkHA <marck.hernandez@epn.edu.ec>
2026-08-30 02:50:14 -0500

    Fix: corrección de baseURL agregando /api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://jack-jill-api.onrender.com/', // Apunta a tu servidor de Node
+    baseURL: 'https://jack-jill-api.onrender.com/api', // Apunta a tu servidor de Node
     withCredentials: true, // ¡Vital para que funcionen las cookies!
 });
