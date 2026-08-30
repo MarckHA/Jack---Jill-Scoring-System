099bd8cc6b2e1b89a6c6ec3508ab024e919ebcdb
MarkHA <marck.hernandez@epn.edu.ec>
2026-08-30 02:40:55 -0500

    Fix: Implementación de HashRouter para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import JudgeDashboard from './pages/JudgeDashboard'; // <-- 1. Importa el componente
+import JudgeDashboard from './pages/JudgeDashboard';
 import OrganizerDashboard from './pages/OrganizerDashboard';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/juez" element={<JudgeDashboard />} />
-
         <Route path="/organizador" element={<OrganizerDashboard />} />
         
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
